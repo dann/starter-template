88a3e74b4b1a24165b2db5f312979ecbe7831f77
Dann <techmemo@gmail.com>
2011-04-17 03:23:49 +0900

    fastpack

---
+++ fastpack.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ -z "$1" ]; then
+	echo "usage: fastpack script.pl > script.packed.pl"
+	exit 0
+fi
+
+fatpack trace "$1"
+fatpack packlists-for `cat fatpacker.trace` > fatpacker.packlists
+fatpack tree `cat fatpacker.packlists`
+[ -d lib ] || mkdir lib
+
+echo "#!/usr/bin/env perl"
+echo
+fatpack file
+cat "$1"
